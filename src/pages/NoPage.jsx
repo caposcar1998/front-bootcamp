3648bc6a9ddb05032b98e178f0e200a1f4f458fd
caposcar1998 <oscarcontrerascap@gmail.com>
2022-07-13 11:31:28 -0500

    Added center page

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,5 +1,14 @@
+import { Button } from '@mui/material'
+import { Container } from '@mui/system'
+
 function NoPage() {
-  return <h1> No page found </h1>
+  return (
+  <Container>
+    <h1> No page found </h1>
+    <h1>404</h1>
+    <Button variant="contained">Reload</Button>
+  </Container>
+  )
 }
 
 export default NoPage
