e5f6ac2f7240fea4d20b5463ec362e9d8dd59a53
Cristian Blandon <cristian.blandon@thebeat.co>
2022-07-13 12:19:58 -0500

    styles: change background color with beat blue

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,11 @@
 import { Typography } from '@mui/material'
 
+const currentYear = new Date().getFullYear()
+
 export default function Footer() {
   return (
-    <Typography color="#23D2AA" paddingBottom={1} variant="h4">
-      BEAT&copy;. All rights reserved.
+    <Typography color="#23D2AA" paddingBottom={1} variant="h6">
+      BEAT {currentYear}&copy;. All rights reserved.
     </Typography>
   )
 }
