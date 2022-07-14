2ef4546dc4605cba781655e1ef145037366843d1
Cristian Blandon <cristian.blandon@thebeat.co>
2022-07-14 11:09:17 -0500

    feat: complete logic for login pending: backend call and redirect

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,25 @@
+import { useState } from 'react'
 import { Box, Button, Paper, TextField } from '@mui/material'
 import { styled } from '@mui/material/styles'
+
+const isValidEmail = (email) => {
+  const re = /^([a-zA-Z0–9._%-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,6})*$/
+  return email !== '' && re.test(String(email).toLowerCase())
+}
+
+const isValidPassword = (password) => {
+  const re =
+    /(?=(.*[0-9]))(?=.*[!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/
+  return password !== '' && re.test(String(password))
+}
+
+async function encryptPassword(password) {
+  const msgUint8 = new TextEncoder().encode(password)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+  return hashHex
+}
 
 const StyledTextField = styled(TextField)({
   marginBottom: '24px',
@@ -27,6 +47,53 @@ const ColoredButton = styled(Button)({
 })
 
 export default function LoginForm() {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [dataError, setDataError] = useState({
+    email: false,
+    password: false
+  })
+
+  const handleChange = (event) => {
+    setDataError({ ...dataError, [event.target.name]: false })
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleError = (event) => {
+    if (event.target.name === 'email') {
+      return setDataError({
+        ...dataError,
+        email: !isValidEmail(event.target.value)
+      })
+    }
+    return setDataError({
+      ...dataError,
+      password: !isValidPassword(event.target.value)
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    if (isValidEmail(userData.email) && isValidPassword(userData.password)) {
+      const encryptedPassword = await encryptPassword(userData.password)
+      // TODO: AXIOS POST request to server
+      // TODO: Redirect to dashboard if ok
+      // TODO: Error message if not ok
+      console.log(`Email: ${userData.email} Password: ${encryptedPassword}`)
+    }
+    if (!isValidEmail(userData.email)) {
+      setDataError((currentData) => ({ ...currentData, email: true }))
+    }
+    if (!isValidPassword(userData.password)) {
+      setDataError((currentData) => ({ ...currentData, password: true }))
+    }
+  }
+
   return (
     <Box
       sx={{
@@ -50,18 +117,37 @@ export default function LoginForm() {
           width="100%"
         >
           <StyledTextField
-            id="outlined-basic"
+            error={dataError.email}
+            helperText={dataError.email ? 'Invalid email address' : ''}
+            name="email"
             label="Email"
             type="email"
             variant="outlined"
+            value={userData.email}
+            onChange={handleChange}
+            onBlur={handleError}
           />
           <StyledTextField
-            id="outlined-basic"
+            error={dataError.password}
+            helperText={
+              dataError.password
+                ? 'Password must be at least 8 characters long and contain at least one number, one special character and one uppercase letter'
+                : ''
+            }
+            name="password"
             label="Password"
             type="password"
             variant="outlined"
+            value={userData.password}
+            onChange={handleChange}
+            onBlur={handleError}
           />
-          <ColoredButton size="large" variant="contained">
+          <ColoredButton
+            onClick={handleSubmit}
+            size="large"
+            type="submit"
+            variant="contained"
+          >
             SIGN IN
           </ColoredButton>
         </Box>
