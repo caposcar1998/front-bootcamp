195e2d8eb38ff7f77d838efa5b192358b0a2c20b
Cristian Blandon <cristian.blandon@thebeat.co>
2022-07-14 12:56:21 -0500

    feat: add Users page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,3 +1,19 @@
+import { UsersTable } from 'components'
+
+const columns = [
+  { id: 'id', label: 'ID' },
+  { id: 'name', label: 'Name' },
+  { id: 'email', label: 'Email' },
+  { id: 'update', label: '' },
+  { id: 'delete', label: '' }
+]
+
+const fakeUsers = [
+  { id: 1, name: 'Leanne Graham', email: 'test@test.com' },
+  { id: 2, name: 'Ervin Howell', email: 'test2@test.com' },
+  { id: 3, name: 'Clementine Bauch', email: 'test3@test.com' }
+]
+
 export default function Users() {
-  return <div>Tablis</div>
+  return <UsersTable columns={columns} users={fakeUsers} />
 }
