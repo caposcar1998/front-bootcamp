efebc0654b1f5d3944f95ef4781a39b824e3567d
Cristian Blandon <cristian.blandon@thebeat.co>
2022-07-15 09:43:19 -0500

    feat: include query param for User page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,9 @@
+// import { useSearchParams } from 'react-router-dom'
 import { UserForm } from 'components'
 
 export default function User() {
+  // const [queryParams] = useSearchParams()
+  // const userId = queryParams.get('id')
+
   return <UserForm />
 }
