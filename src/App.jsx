796584a796930663d5725702039ed4dfdef45119
Cristian Blandon <cristian.blandon@thebeat.co>
2022-07-15 11:22:17 -0500

    feat: protect routes with auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { Login, User, Users, NoPage } from 'pages'
 import { Layout } from 'components'
 
 function App() {
+  const loggedUser = sessionStorage.getItem('loggedUser')
+
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
       <Layout>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="user" element={<User />} />
-            <Route path="users" element={<Users />} />
+            <Route path="/" element={loggedUser ? <Users /> : <Login />} />
+            <Route path="user" element={loggedUser ? <User /> : <NoPage />} />
+            <Route path="users" element={loggedUser ? <Users /> : <NoPage />} />
             <Route path="*" element={<NoPage />} />
           </Routes>
         </BrowserRouter>
