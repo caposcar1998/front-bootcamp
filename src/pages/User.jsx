b86c17b74659a939cbf552b3b18907c4ef216385
Cristian Blandon <cristian.blandon@thebeat.co>
2022-07-15 12:51:03 -0500

    feat: add getUser for edit page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,32 @@
-// import { useSearchParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
 import { UserForm } from 'components'
 
 export default function User() {
-  // const [queryParams] = useSearchParams()
-  // const userId = queryParams.get('id')
+  const [queryParams] = useSearchParams()
+  const userId = queryParams.get('id')
 
-  return <UserForm />
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if (userId) {
+      setLoading(true)
+      axios
+        .get(`/api/v1/users/${userId}`)
+        .then(({ data }) => {
+          setUser(data)
+        })
+        .catch(() => {
+          setUser({})
+          setError(true)
+        })
+    }
+  }, [userId])
+
+  return (
+    <UserForm loadingUser={loading} errorFetchingUser={error} user={user} />
+  )
 }
