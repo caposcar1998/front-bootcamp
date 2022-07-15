78ae884af0c20dbe6469660d2055d4e7cf966f0b
caposcar1998 <oscarcontrerascap@gmail.com>
2022-07-15 10:55:37 -0500

    Added navigate

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,12 +1,18 @@
 import { Button } from '@mui/material'
 import { Container } from '@mui/system'
+import { useNavigate } from "react-router-dom";
 
 export default function NoPage() {
+  function reloadPage() {
+    const navigate = useNavigate()
+    const path = '/'
+    navigate(path)
+  }
   return (
   <Container>
     <h1> No page found </h1>
     <h1>404</h1>
-    <Button variant="contained">Reload</Button>
+    <Button variant="contained" onClick={reloadPage}>Reload</Button>
   </Container>
   )
 }
